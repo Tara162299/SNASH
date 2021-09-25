5a4b534f7fcce597d8774541f54cdd648595cdd8
Zach <zbk3@case.edu>
2021-09-25 05:45:04 -0400

    Multiple page and button functionality added.
    Metadata retrieval still unimplemented, and path retrieval broken.

--- src/main/java/com/snash/Control.java
+++ src/main/java/com/snash/Control.java
@@ -10,5 +10,10 @@ public class Control {
     public static void main(String[] args) {
         MTableUI table = new MTableUI();
         table.runUI(args);
+        // Not working yet- all of table's variables seem to be set to null after closing the stage.
+        // Need to find a way to store data, close the stage, then have the data retrieved.
+        String pathFile = table.getPath();
+        System.out.println(pathFile);
+
     }
 }
--- src/main/java/com/snash/MTableUI.java
+++ src/main/java/com/snash/MTableUI.java
@@ -10,59 +10,88 @@ import java.util.ArrayList;
 
 public class MTableUI extends Application {
 
-    // This should be a Metadata object, but that class isn't written yet.
-    private Object metadata;
-    private String filepath;
+    private Stage stage;
 
     private ArrayList<TablePage> pages = new ArrayList<>();
-    private int currentPage = 0;
+    private int currentPageNumber = 0;
+    private String finalPathFieldText = null;
 
-    void runUI(String[] args){
+    public String getPath(){
+        return finalPathFieldText;
+    }
+
+    public void runUI(String[] args){
         launch(args);
     }
 
     @Override
-    public void start(Stage stage) {
-        stage.setMaximized(true);
-
+    public void start(Stage inputStage) {
+        stage = inputStage;
         stage.setTitle("Enter Metadata");
+        pages.add(newPage(0));
+        showPage(0);
+    }
 
-        pages.add(newPage());
+    public void close(){
+        stage.close();
+    }
 
+    void showPage(int pageNumber){
         StackPane root = new StackPane();
-        root.getChildren().add(pages.get(0).getGrid());
+        root.getChildren().add(pages.get(pageNumber).getGrid());
         stage.setScene(new Scene(root));
         stage.show();
     }
 
-    void previousPage(){
+    // This is a replacement for "nextPage" and "previousPage" to reduce duplicate code.
+    // Safeguards are in place for bad page numbers, but for now, stick to one more or less than the current page.
+    void moveToPage(int pageNumber){
+        if (pageNumber < 0){
+            return;
+        }
 
+        String pathFieldText = pathFieldText(currentPageNumber);
+
+        // If the page number is too high, add more pages.
+        for (int i = pages.size(); i <= pageNumber; i++){
+            TablePage newPage = newPage(i);
+            pages.add(newPage);
+        }
+        pages.get(pageNumber).setPathFieldText(pathFieldText);
+        showPage(pageNumber);
+        currentPageNumber = pageNumber;
     }
 
-    void nextPage(){
-
+    void submit() {
+        finalPathFieldText = pathFieldText(currentPageNumber);
+        stage.close();
     }
 
-    void submit(){
-
-    }
-
-    private TablePage newPage(){
-        Button doneButton = new Button();
-        doneButton.setText("Submit Metadata");
+    // Returns a new page with the specified pageNumber.
+    // Page number is passed (rather than read from global) as not to be dependent on when currentPageNumber is incremented.
+    private TablePage newPage(int pageNumber){
+        Button doneButton = new Button("Submit Metadata");
         doneButton.setOnAction((event) ->
                 submit());
 
-        Button previousButton = new Button();
-        previousButton.setText("Previous");
+        Button previousButton = new Button("Previous");
         previousButton.setOnAction((event) ->
-                previousPage());
+                moveToPage(pageNumber - 1));
 
-        Button nextButton = new Button();
-        nextButton.setText("Next");
+        Button nextButton = new Button("Next");
         nextButton.setOnAction((event) ->
-                nextPage());
+                moveToPage(pageNumber + 1));
 
-        return new TablePage(previousButton, nextButton, doneButton);
+        TablePage outputPage = new TablePage();
+        outputPage.setPreviousButton(previousButton);
+        outputPage.setNextButton(nextButton);
+        outputPage.setDoneButton(doneButton);
+        outputPage.setPageNumber(pageNumber);
+
+        return outputPage;
+    }
+
+    private String pathFieldText(int pageNumber){
+        return pages.get(pageNumber).getPathFieldText();
     }
 }
